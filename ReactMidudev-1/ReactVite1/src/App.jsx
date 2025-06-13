bbe374da18050426f0726d2eae419cf2772fdf3e
JuanPGO <pablogomez.jg@gmail.com>
2025-06-13 10:23:14 -0500

    Enhance TwitterFollowCard and App components with improved follow button functionality and styling. Update App.css for better visual feedback on follow state and streamline user data handling in App.jsx.

--- ReactMidudev-1/ReactVite1/src/App.jsx
+++ ReactMidudev-1/ReactVite1/src/App.jsx
@@ -1,61 +1,115 @@
+//! Ejemplos realizados durante el video
+// import { useState } from 'react'
+// import './App.css'
+// import { TwitterFollowCard } from './TwitterFollowCard'
+// export function App() {
+//   //   const formatUserName = (userName) => `${userName}`
+//   const formatUserName = (userName) => <span>@{userName}</span>
+
+//   // arreglo necesario para el ejemplo de llamar o asignar varios props a la vez
+//   const minudev = {
+//     isFollowing: true,
+//     userName: 'midudev',
+//     formatUserName: formatUserName,
+//   }
+//   const pheralb = {
+//     isFollowing: false,
+//     userName: 'pheralb',
+//     formatUserName: formatUserName,
+//   }
+
+//   const [isFollowing, setIsFollowing] = useState(false)
+
+//   console.log('[app] render with isFollowing: ', isFollowing)
+
+//   return (
+//     // <section className="App">
+//     //   <TwitterFollowCard
+//     //     formatUserName={formatUserName}
+//     //     name="Miguel Angel Duran"
+//     //     userName="midudev"
+//     //     isFollowing={true}
+//     //   />
+//     //   <TwitterFollowCard
+//     //     formatUserName={formatUserName}
+//     //     name="Miguel Angel Duran"
+//     //     userName="pheralb"
+//     //     isFollowing={false}
+//     //   />
+//     // </section>
+
+//     //? ejemplo para el manejo de los children
+//     <section className="App">
+//       <TwitterFollowCard
+//         formatUserName={formatUserName}
+//         userName="midudev"
+//         InitialIsFollowing={isFollowing}
+//       >
+//         Miguel Angel Duran
+//       </TwitterFollowCard>
+
+//       <button onClick={() => setIsFollowing(!isFollowing)}>
+//         Cambiar estado de App
+//       </button>
+//       {/* <TwitterFollowCard
+//         formatUserName={formatUserName}
+//         userName="pheralb"
+//         InitialIsFollowing={false}
+//       >
+//         Juan Pablo Gómez Arcila
+//       </TwitterFollowCard> */}
+//     </section>
+
+//     //? Ejemplo para el manejo del llamado a varios props a la vez sin tener que escribirlos
+
+//     // <section className="App">
+//     //   <TwitterFollowCard {...minudev}>Miguel Angel Duran</TwitterFollowCard>
+//     //   <TwitterFollowCard {...pheralb}>
+//     //     Juan Pablo Gómez Arcila
+//     //   </TwitterFollowCard>
+//     // </section>
+//   )
+// }
+
+//! Resultado final del archivo
 import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
-export function App() {
-  //   const formatUserName = (userName) => `${userName}`
-  const formatUserName = (userName) => <span>@{userName}</span>
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
-  // arreglo necesario para el ejemplo de llamar o asignar varios props a la vez
-  const minudev = {
-    isFollowing: true,
+const users = [
+  {
     userName: 'midudev',
-    formatUserName: formatUserName,
-  }
-  const pheralb = {
-    isFollowing: false,
+    name: 'Miguel Ángel Durán',
+    isFollowing: true,
+  },
+  {
     userName: 'pheralb',
-    formatUserName: formatUserName,
-  }
+    name: 'Pablo H.',
+    isFollowing: false,
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true,
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false,
+  },
+]
+
+export function App() {
   return (
-    // <section className="App">
-    //   <TwitterFollowCard
-    //     formatUserName={formatUserName}
-    //     name="Miguel Angel Duran"
-    //     userName="midudev"
-    //     isFollowing={true}
-    //   />
-    //   <TwitterFollowCard
-    //     formatUserName={formatUserName}
-    //     name="Miguel Angel Duran"
-    //     userName="pheralb"
-    //     isFollowing={false}
-    //   />
-    // </section>
-
-    //? ejemplo para el manejo de los children
     <section className="App">
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="midudev"
-        isFollowing
-      >
-        Miguel Angel Duran
-      </TwitterFollowCard>
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="pheralb"
-        isFollowing={false}
-      >
-        Juan Pablo Gómez Arcila
-      </TwitterFollowCard>
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
     </section>
-
-    //? Ejemplo para el manejo del llamado a varios props a la vez sin tener que escribirlos
-
-    // <section className="App">
-    //   <TwitterFollowCard {...minudev}>Miguel Angel Duran</TwitterFollowCard>
-    //   <TwitterFollowCard {...pheralb}>
-    //     Juan Pablo Gómez Arcila
-    //   </TwitterFollowCard>
-    // </section>
   )
 }
--- ReactMidudev-1/ReactVite1/src/TwitterFollowCard.jsx
+++ ReactMidudev-1/ReactVite1/src/TwitterFollowCard.jsx
@@ -1,19 +1,64 @@
+//! Ejemplos realizados durante el video
+// import { useState } from 'react'
+
+// export function TwitterFollowCard({
+//   children,
+//   formatUserName,
+//   userName,
+//   InitialIsFollowing,
+// }) {
+//   //   const addAt = (userName) => `@${userName}`
+
+//   const state = useState(false) // por defecto es falso
+//   //? se usa una forma sencilla con la destructuración.
+//   //   const isFollowing = state[0]
+//   //   const setIsFollowing = state[1]
+
+//   const [isFollowing, setIsFollowing] = useState(InitialIsFollowing)
+
+//   console.log('[TwitterFollowCard] render with userName: ', userName)
+
+//   const text = isFollowing ? 'Siguiendo' : 'Seguir'
+//   const buttonClassName = isFollowing
+//     ? 'tw-followCard-button is-following'
+//     : 'tw-followCard-button'
+
+//   const handleClick = () => {
+//     setIsFollowing(!isFollowing)
+//   }
+
+//   return (
+//     <article className="tw-followCard">
+//       <header className="tw-followCard-header">
+//         <img
+//           className="tw-followCard-avatar"
+//           alt="El avatar de midudev"
+//           src={`https://unavatar.io/${children}`}
+//         />
+//         <div className="tw-followCard-info">
+//           <strong>{children}</strong>
+//           <span className="tw-followCard-infoUserName">
+//             {formatUserName(userName)}
+//           </span>
+//         </div>
+//       </header>
+//       <aside>
+//         <button className={buttonClassName} onClick={handleClick}>
+//           <span className="tw-followCard-text">{text}</span>
+//           <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+//         </button>
+//       </aside>
+//     </article>
+//   )
+// }
+
+//! Resultado final del archivo
 import { useState } from 'react'
 
-export function TwitterFollowCard({
-  children,
-  formatUserName,
-  userName,
-  //   isFollowing,
-}) {
-  //   const addAt = (userName) => `@${userName}`
+export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
-  const state = useState(false) // por defecto es falso
-  //? se usa una forma sencilla con la destructuración.
-  //   const isFollowing = state[0]
-  //   const setIsFollowing = state[1]
-
-  const [isFollowing, setIsFollowing] = useState(false)
+  console.log('[TwitterFollowCard] render with userName: ', userName)
 
   const text = isFollowing ? 'Siguiendo' : 'Seguir'
   const buttonClassName = isFollowing
@@ -30,18 +75,18 @@ export function TwitterFollowCard({
         <img
           className="tw-followCard-avatar"
           alt="El avatar de midudev"
-          src={`https://unavatar.io/${children}`}
+          src={`https://unavatar.io/${userName}`}
         />
         <div className="tw-followCard-info">
           <strong>{children}</strong>
-          <span className="tw-followCard-infoUserName">
-            {formatUserName(userName)}
-          </span>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
         </div>
       </header>
+
       <aside>
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
         </button>
       </aside>
     </article>
